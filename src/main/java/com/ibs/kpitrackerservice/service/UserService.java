a52b42a34fcd6e257e0b29cc1d7b498e5677b29d
Lidhiya Sajan <lsajan@LE1076.local>
2019-12-20 18:08:43 +0530

    Dummy commit
    Signed-off-by: Lidhiya Sajan <lsajan@LE1076.local>

--- src/main/java/com/ibs/kpitrackerservice/service/UserService.java
+++ src/main/java/com/ibs/kpitrackerservice/service/UserService.java
@@ -1,7 +1,18 @@
 package com.ibs.kpitrackerservice.service;
 
+import com.ibs.kpitrackerservice.model.User;
+import com.ibs.kpitrackerservice.respository.UserRepository;
+import org.apache.el.stream.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> getTeam(String id) {
+        return userRepository.findById(id);
+    }
 }
