591550267a5d7c6e8f8ecea768990ba628c6c776
asraj <v-asraj@expedia.com>
2020-01-08 21:30:52 +0530

    Report module

--- src/main/java/com/ibs/kpitrackerservice/respository/ReportRepository.java
+++ src/main/java/com/ibs/kpitrackerservice/respository/ReportRepository.java
@@ -2,6 +2,14 @@ package com.ibs.kpitrackerservice.respository;
 
 import com.ibs.kpitrackerservice.model.Report;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ReportRepository extends MongoRepository<Report, String> {
+
+    @Query("select report from Report report where report.startDate >= :startDate and report.endDate <= :endDate")
+    List<Report> findByStartDateAfterAndEndDateBefore(
+            @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
